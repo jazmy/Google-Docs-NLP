dddce5de5e1288f4197a9d333778fcc4945cf36d
Jasmine Robinson <28543740+jazmy@users.noreply.github.com>
2022-04-24 17:25:02 -0700

    Fixed the all links function

--- generateNLPLinksSheets.js
+++ generateNLPLinksSheets.js
@@ -8,11 +8,11 @@ To run this script you will need a Google Cloud Natural Language API Key.
 
 // The function will take the document body and send it to the Google Cloud Natural Language API
 // Documentation: https://cloud.google.com/natural-language/docs/reference/rest
-function retrieveEntitySentiment(line) {
+function Main(line) {
     var doc = DocumentApp.getActiveDocument();
     const body = doc.getBody();
     const docText = body.getText();
-    var apiKey = "XXXXXXXXXXXXXXXXXXX"; // Must provide your own API key
+    var apiKey = "XXXXXXXXXXXXXXXXXXXXX"; // Must provide your own API key
 
     var apiEndpoint = 'https://language.googleapis.com/v1/documents:analyzeEntitySentiment?key=' + apiKey;
 
@@ -85,7 +85,7 @@ function createSpreadsheet(nlData) {
     var links = getAllLinks();
     // Loop through the array and write to rows on the spreadsheet
     links.forEach(function (link) {
-        var row = [link];
+        var row = [link.url];
         newValues1.push(row);
     });
     if (newValues1.length) {
@@ -95,13 +95,135 @@ function createSpreadsheet(nlData) {
 
 }
 
-// Returns a flat array of links which appear in the active document's body. 
-// https://urlregex.com/
-function getAllLinks() {
-    var doc = DocumentApp.getActiveDocument();
-    const body = doc.getBody();
-    const docText = body.getText();
-    var urlRegex = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/g
-    //console.log(docText.match(urlRegex))
-    return docText.match(urlRegex);
+
+/**
+ * Pasted from: https://stackoverflow.com/questions/18727341/get-all-links-in-a-document
+ * Returns a flat array of links which appear in the active document's body. 
+ * Each link is represented by a simple Javascript object with the following 
+ * keys:
+ *   - "section": {ContainerElement} the document section in which the link is
+ *     found. 
+ *   - "isFirstPageSection": {Boolean} whether the given section is a first-page
+ *     header/footer section.
+ *   - "paragraph": {ContainerElement} contains a reference to the Paragraph 
+ *     or ListItem element in which the link is found.
+ *   - "text": the Text element in which the link is found.
+ *   - "startOffset": {Number} the position (offset) in the link text begins.
+ *   - "endOffsetInclusive": the position of the last character of the link
+ *      text, or null if the link extends to the end of the text element.
+ *   - "url": the URL of the link.
+ *
+ * @param {boolean} mergeAdjacent Whether consecutive links which carry 
+ *     different attributes (for any reason) should be returned as a single 
+ *     entry.
+ * 
+ * @returns {Array} the aforementioned flat array of links.
+ */
+function getAllLinks(mergeAdjacent) {
+  var links = [];
+
+  var doc = DocumentApp.getActiveDocument();
+
+
+  iterateSections(doc, function(section, sectionIndex, isFirstPageSection) {
+    if (!("getParagraphs" in section)) {
+      // as we're using some undocumented API, adding this to avoid cryptic
+      // messages upon possible API changes.
+      throw new Error("An API change has caused this script to stop " + 
+                      "working.\n" +
+                      "Section #" + sectionIndex + " of type " + 
+                      section.getType() + " has no .getParagraphs() method. " +
+        "Stopping script.");
+    }
+
+    section.getParagraphs().forEach(function(par) { 
+      // skip empty paragraphs
+      if (par.getNumChildren() == 0) {
+        return;
+      }
+
+      // go over all text elements in paragraph / list-item
+      for (var el=par.getChild(0); el!=null; el=el.getNextSibling()) {
+        if (el.getType() != DocumentApp.ElementType.TEXT) {
+          continue;
+        }
+
+        // go over all styling segments in text element
+        var attributeIndices = el.getTextAttributeIndices();
+        var lastLink = null;
+        attributeIndices.forEach(function(startOffset, i, attributeIndices) { 
+          var url = el.getLinkUrl(startOffset);
+
+          if (url != null) {
+            // we hit a link
+            var endOffsetInclusive = (i+1 < attributeIndices.length? 
+                                      attributeIndices[i+1]-1 : null);
+
+            // check if this and the last found link are continuous
+            if (mergeAdjacent && lastLink != null && lastLink.url == url && 
+                  lastLink.endOffsetInclusive == startOffset - 1) {
+              // this and the previous style segment are continuous
+              lastLink.endOffsetInclusive = endOffsetInclusive;
+              return;
+            }
+
+            lastLink = {
+              "section": section,
+              "isFirstPageSection": isFirstPageSection,
+              "paragraph": par,
+              "textEl": el,
+              "startOffset": startOffset,
+              "endOffsetInclusive": endOffsetInclusive,
+              "url": url
+            };
+console.log(lastLink.url)
+            links.push(lastLink);
+          }        
+        });
+      }
+    });
+  });
+
+
+  return links;
+}
+
+/**
+ * Calls the given function for each section of the document (body, header, 
+ * etc.). Sections are children of the DocumentElement object.
+ *
+ * @param {Document} doc The Document object (such as the one obtained via
+ *     a call to DocumentApp.getActiveDocument()) with the sections to iterate
+ *     over.
+ * @param {Function} func A callback function which will be called, for each
+ *     section, with the following arguments (in order):
+ *       - {ContainerElement} section - the section element
+ *       - {Number} sectionIndex - the child index of the section, such that
+ *         doc.getBody().getParent().getChild(sectionIndex) == section.
+ *       - {Boolean} isFirstPageSection - whether the section is a first-page
+ *         header/footer section.
+ */
+function iterateSections(doc, func) {
+  // get the DocumentElement interface to iterate over all sections
+  // this bit is undocumented API
+  var docEl = doc.getBody().getParent();
+
+  var regularHeaderSectionIndex = (doc.getHeader() == null? -1 : 
+                                   docEl.getChildIndex(doc.getHeader()));
+  var regularFooterSectionIndex = (doc.getFooter() == null? -1 : 
+                                   docEl.getChildIndex(doc.getFooter()));
+
+  for (var i=0; i<docEl.getNumChildren(); ++i) {
+    var section = docEl.getChild(i);
+
+    var sectionType = section.getType();
+    var uniqueSectionName;
+    var isFirstPageSection = (
+      i != regularHeaderSectionIndex &&
+      i != regularFooterSectionIndex && 
+      (sectionType == DocumentApp.ElementType.HEADER_SECTION ||
+       sectionType == DocumentApp.ElementType.FOOTER_SECTION));
+
+    func(section, i, isFirstPageSection);
+  }
 }
